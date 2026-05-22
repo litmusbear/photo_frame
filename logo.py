0872666318e5dd18e28c663a893ff53c3e8d34bc
litmusbear <siwho3@gmail.com>
2026-05-22 11:53:03 +0900

    Update logo.py

--- logo.py
+++ logo.py
@@ -9,9 +9,16 @@ def logo(picture):
         logo_file = "logos/sony.png"
     elif "CANON" in camera_model:
         logo_file = "logos/canon.png"
-    elif "IPHONE" in camera_model:
+    elif "IPHONE" in camera_model or "IPAD" in camera_model:
         logo_file = "logos/apple.png"
-    elif "SAMSUNG" in camera_model:
+    elif "SAMSUNG" in camera_model or "SM" in camera_model:
         logo_file = "logos/samsung.png"
+    elif "FUJIFILM" in camera_model:
+        logo_file = "logos/fujifilm.png"
+
+    if not logo_file:
+                exif_str = str(image._getexif()).upper()
+                if "SONY" in exif_str or "ILCE" in exif_str or "ZV-" in exif_str:
+                    logo_file = "logos/sony.png"
 
     return logo_file
